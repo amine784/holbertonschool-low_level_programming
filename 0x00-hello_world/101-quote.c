b7c98c59f10e274a79d715f20defddd71d471e6a
amine784 <1066@holbertonschool.com>
2019-09-18 16:33:22 +0100

    holberton

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
-#include"stdio.h"
+#include "stdio.h"
 #include "string.h"
 #include "unistd.h"
 /**
- *main -block 
- *return:1
+ * main - main block
+ * Return: 1
  */
-int main (void)
+int main(void)
 {
-  int len;
-  len=strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
-  return(1);
+int len;
+len = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
+return (1);
 }
