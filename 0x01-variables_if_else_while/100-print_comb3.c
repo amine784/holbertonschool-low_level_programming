b5773d10d576685fb1f0a3cca66e4380d6c174e6
amine784 <1066@holbertonschool.com>
2019-09-19 16:47:13 +0100

    c programming

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - 
+ * comb3
+ * Return: 0
+ */
+int main(void)
+{
+  int i,j;
+for (i = 48; i <= 57; i++)
+for (j = 48; j <= 57; j++)
+if (i < j)
+{
+putchar(i);
+putchar(j);
+if (i != 56 || j != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
