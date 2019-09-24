abc10a69bbb57741958a4fe767e5517735bd6ec9
amine784 <1066@holbertonschool.com>
2019-09-24 10:09:46 +0100

    c programming

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - print every minute of the day
+ *
+ *
+ * Return: all the minutes
+ */
+
+void jack_bauer(void)
+{
+int a, b;
+for (a = 0; a <= 23; a++)
+{
+for (b = 0; b <= 59; b++)
+{
+_putchar(a / 10 + '0');
+_putchar(a % 10 + '0');
+_putchar(':');
+_putchar(b / 10 + '0');
+_putchar(b % 10 + '0');
+_putchar('\n');
+}
+}
+}
