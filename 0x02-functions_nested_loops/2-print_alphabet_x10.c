fa99036d7f8359130a4cf86dd143f91e6c70607d
amine784 <1066@holbertonschool.com>
2019-09-24 08:47:47 +0100

    holberton

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
 #include "holberton.h"
 /**
- * main - entry point
- * alternative way 
- * Return: Always 0 (Success)
- */
+*main - entry point.
+*Return: Always 0 (Success).
+*/
 void print_alphabet_x10(void)
 {
 int i, j;
