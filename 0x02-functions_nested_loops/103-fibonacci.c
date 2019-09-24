5d7f1fafc390ff5b0226e90225ec40a96bf071b7
amine784 <1066@holbertonschool.com>
2019-09-24 16:29:27 +0100

    holberton

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -27,3 +27,25 @@ int main(void)
 	printf("%ld\n", sum);
 	return (0);
 }
+#include <stdio.h>
+/**
+ * main - print fibonacci
+ * fibonacci s
+ * Return: always 0
+ */
+int main(void)
+{
+long int u0 = 1, u1 = 2, sum = 2;
+int ip;
+
+for (ip = 1; ip < 31; ip++)
+{
+if ((u0 + u1) % 2 == 0)
+sum += u0 + u1;
+
+u1 = u0 + u1;
+u0 = u1 - u0;
+}
+printf("%ld\n", sum);
+return (0);
+}
