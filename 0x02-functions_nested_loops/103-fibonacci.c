03a61a45ecbca8b762a107392ee230015b9d3dbf
amine784 <1066@holbertonschool.com>
2019-09-24 16:32:31 +0100

    c programming

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,32 +1,3 @@
-include "stdio.h"
-
-/**
- * main - sum the even  numbers  below
- * 4000000 in  fibobnachi seq
- * Description: fibobnachi numbers
- * Return: 0
- */
-
-int main(void)
-{
-	long int a  = 0;
-	long int b  = 0;
-	long int c  = 1;
-	long int sum = 0;
-
-	while (b < 4000000)
-	{
-		b = a + c;
-		a = c;
-		c = b;
-		if (b % 2 == 0)
-		{
-			sum  = sum  + b;
-		}
-	}
-	printf("%ld\n", sum);
-	return (0);
-}
 #include <stdio.h>
 /**
  * main - print fibonacci
@@ -49,3 +20,4 @@ u0 = u1 - u0;
 printf("%ld\n", sum);
 return (0);
 }
+
