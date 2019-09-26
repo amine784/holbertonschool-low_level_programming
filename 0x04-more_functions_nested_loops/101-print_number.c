346d5d4da88a9ba44299631f337f5bc7786e8169
amine784 <1066@holbertonschool.com>
2019-09-26 16:59:18 +0100

    c programming

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,43 +1,19 @@
 #include "holberton.h"
 /**
- * print_number - function to print integers
- *
- * @n: int type
- * Return: return integer values
- */
-
+* print_number - func print int
+*
+* @n: int type
+* Return: integer value
+**/
 void print_number(int n)
 {
-	int a, rm, length, c;
-	int m = n;
-	int pc = 0;
-	int pr;
-
-	if (n == 0)
-	{
-		_putchar('0');
-	}
-	if (n < 0)
-	{
-		_putchar('-');
-	}
-	for (length = 0; n != 0; length++)
-	{
-		n /= 10;
-	}
-	for (pr = 1; pc < (length - 1); pc++)
-	{
-		pr *= 10;
-	}
-	for (c = 0; c < length; c++)
-	{
-		a = m;
-		if (m < 0)
-			_putchar(((m / pr) * -1) + '0');
-		else
-			_putchar((m / pr) + '0');
-		rm = a % pr;
-		pr /= 10;
-		m = rm;
-	}
+unsigned int num = n;
+if (n < 0)
+{
+_putchar('-');
+num = -num;
+}
+if ((num / 10) > 0)
+print_number(num / 10);
+_putchar((num % 10) + '0');
 }
