b2435d29f5bd0d1171b10d34499b4c7aa583c46d
amine784 <1066@holbertonschool.com>
2019-09-26 14:42:10 +0100

    c programming

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
  * print_line -n is the number of char.
- *
+ *@n:int type
  * Return 0.
  */
-void print_line(void)
+void print_line(int n);
 {
 int i;
 for (i = 0; i < n; i++)
