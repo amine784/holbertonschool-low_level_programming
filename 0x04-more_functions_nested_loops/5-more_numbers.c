b7c207e3cf32c245b62d17a5d772fdd933d3e06b
amine784 <1066@holbertonschool.com>
2019-09-26 13:46:19 +0100

    c programming

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,4 +4,19 @@
  */
 void more_numbers(void)
 {
+int i, j, k;
+for (i = 0; i <10; i++)
+{
+for (j = 0; j <=14; j++)
+{
+if (j < 10)
+k = j;
+else
+k = j / 10;
+putchar(k + '0');
+if (j > 9)
+putchar(j % 10 + '0');
+}
+putchar('\n');
+}
 }
