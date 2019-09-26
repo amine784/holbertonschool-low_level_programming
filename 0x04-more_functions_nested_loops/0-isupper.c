c2bc4986c23a7e17bf68c8fb016273624e97b278
amine784 <1066@holbertonschool.com>
2019-09-26 08:25:14 +0100

    c programming

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 /**
- * _isupper - checks if character is upercase, return 1 is yes and 0 if not.
+ * _isupper - checks if character is uppercase, return 1 is yes and 0 if not.
  * @c: is char type
  * Return: 1 if c is uppercase  and 0 otherwise.
  */
 
-int _islower(int c)
+int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 		return (1);
