113ad97365f3abc00eda9339e90fef7abf21991e
amine784 <1066@holbertonschool.com>
2019-09-26 13:20:11 +0100

    c programming

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,15 @@
 #include "holberton.h"
 /**
-*print_most_numbers - prints numbers 0..9 /2 and 4.
-*
-*
-*Return: 0(success).
-*/
+ * print_most_numbers - print most numbers 0-9 ex (2 3 4).
+ */
 void print_most_numbers(void)
 {
-int  i;
-for (i = 0 ; i < 10 ; i++)
-{
-if ((i == 2) && (i == 4))
-{
-i++;
-}
-_putchar(i + '0');
-}
-_putchar ('\n');
-return (0);
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
 }
