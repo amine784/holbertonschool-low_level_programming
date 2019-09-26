c0dda33636f6db243e1ff8f3e7eb8cfb51454d20
amine784 <1066@holbertonschool.com>
2019-09-26 16:48:27 +0100

    c programming

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,33 +1,43 @@
 #include "holberton.h"
 /**
-* print_number - prints # using _putchar func
-* @n:  integer to print
-*
-* Return: (void)
-*/
+ * print_number - function to print integers
+ *
+ * @n: int type
+ * Return: return integer values
+ */
+
 void print_number(int n)
 {
-int cp, rm, size = 1, m = n % 10;
-n /= 10;
-cp = n;
-if (m < 0)
-{
-m *= -1, cp *= -1, n *= -1;
-_putchar('-');
-}
-if (cp > 0)
-{
-while (cp / 10 != 0)
-{
-cp /= 10, size *= 10;
-}
-while (size > 0)
-{
-rm = n / size;
-_putchar('0' + rm);
-n -= rm *size;
-size /= 10;
-}
-}
-_putchar('0' + m);
+	int a, rm, length, c;
+	int m = n;
+	int pc = 0;
+	int pr;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+	for (length = 0; n != 0; length++)
+	{
+		n /= 10;
+	}
+	for (pr = 1; pc < (length - 1); pc++)
+	{
+		pr *= 10;
+	}
+	for (c = 0; c < length; c++)
+	{
+		a = m;
+		if (m < 0)
+			_putchar(((m / pr) * -1) + '0');
+		else
+			_putchar((m / pr) + '0');
+		rm = a % power;
+		pr /= 10;
+		m = rm;
+	}
 }
