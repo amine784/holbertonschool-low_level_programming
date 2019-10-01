bcc3459a6d69c6757bf588994f2ad83701142e0b
amine784 <1066@holbertonschool.com>
2019-10-01 11:17:25 +0100

    pointer

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,4 +1,5 @@
 void reset_to_98(int *n);
+int _putchar(char c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
@@ -9,3 +10,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
