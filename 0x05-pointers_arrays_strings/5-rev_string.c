78c4e5bf1d339e2347e3c2fd3951d774074ca1a9
amine784 <1066@holbertonschool.com>
2019-10-01 13:59:38 +0100

    pointer

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,3 +3,25 @@
  *rev_string- reverse a string.
  *@s:char type.
  *Return: Always 0.
+ */
+void rev_string(char *s)
+{
+int i;
+i = 0;
+int k;
+int l;
+int j;
+l = 0;
+while (s[i] != 0)
+{
+i++;
+}
+j = i--;
+for (j = 0; j < i / 2 ; j++)
+{
+k = s[l];
+s[l] = s[j];
+s[i] = k;
+l--;
+}
+}
