9140df05af33b696d5c2985c44096eb8de2b1855
amine784 <1066@holbertonschool.com>
2019-10-01 11:55:51 +0100

    pointer

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+*strlen -return length of string.
+*@s:char type.
+*return always 0.
+*check the code for holberton school student
+int _strlen(char *s)
+{
+int i;
+for(i=0,s[i] != 0;i++;)
+return (i);
