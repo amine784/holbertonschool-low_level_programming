d8df113d7cb698c125a9a5cfe0b406fdf05e9481
amine784 <1066@holbertonschool.com>
2019-10-09 14:15:25 +0100

    recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,13 +6,13 @@
 **/
 int factorial(int n)
 {
-if(n<0)
+if (n < 0)
 {
-return(-1);
+return (-1);
 }
-if(n <= 1)
+if (n <= 1)
 {
 return (1);
 }
-return n * factorial(n - 1);
+return (n * factorial(n - 1));
 }
