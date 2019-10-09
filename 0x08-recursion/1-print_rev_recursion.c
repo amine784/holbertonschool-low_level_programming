7cf697ec7c90dc8c903af3ebb74bc595bd715df9
amine784 <1066@holbertonschool.com>
2019-10-09 13:12:19 +0100

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,3 +1,15 @@
 #include "holberton.h"
 /**
-*
+* _print_rev_recursion(char *s)-function that prints a string in rev
+*followed by a new line.
+*@s: type char :string to print.
+*Return: string s in reverse.
+**/
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
