5797e295c1869ef9597f1b3d006b78ee2e537e15
amine784 <1066@holbertonschool.com>
2019-10-09 14:09:08 +0100

    recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+*factorial-calculate the facturual of n.
+*@n:int type.
+*Return: factorial of n.
+**/
+int factorial(int n)
+{
+if(n<0)
+{
+return(-1);
+}
+if(n <= 1)
+{
+return (1);
+}
+return n * factorial(n - 1);
+}
