2f30641d0074892dd757a2f698739c7a0301cfa1
amine784 <1066@holbertonschool.com>
2019-10-13 10:54:54 -0700

    holberton

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
 *@argv:char pointer that contain the name of argument
 *Return: always 0.
 **/
-int main(int argc,  char *argv[] __attribute__((unused)) )
+int main(int argc,  char *argv[] __attribute__((unused)))
 {
 printf("%d\n", argc - 1);
 return (0);
