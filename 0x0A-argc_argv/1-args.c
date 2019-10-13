deaa1d15127c961f3b36cee73216bc13e672bbff
amine784 <1066@holbertonschool.com>
2019-10-13 10:52:41 -0700

    holberton

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main- program that prints number of arguments passed into it
+*@argc:int type that contain the nbre of arguments.
+*@argv:char pointer that contain the name of argument
+*Return: always 0.
+**/
+int main(int argc,  char *argv[] __attribute__((unused)) )
+{
+printf("%d\n", argc - 1);
+return (0);
+}
