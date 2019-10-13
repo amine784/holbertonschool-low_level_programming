74e2eddf05bc2dd81e059868d3f85584268264c9
amine784 <1066@holbertonschool.com>
2019-10-13 10:35:44 -0700

    biblio

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main- program that prints its name if
+*you rename it wil print the new name
+*@argc:int type that contain the nbre of arguments.
+*@argv:char pointer that contain the name of argument
+*Return: always 0.
+**/
+int main(int argc__attribute__((unused)),  char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
