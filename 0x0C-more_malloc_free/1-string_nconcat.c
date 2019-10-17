973ead96594b7d56d71d212c56ea6d1f53ba45d4
amine784 <1066@holbertonschool.com>
2019-10-17 11:58:08 -0700

    holberton

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,22 +12,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 unsigned int i, j, k, l;
 char *p;
 if (s1 != NULL)
-{
 for (i = 0; s1[i] != '\0'; i++)
 ;
-}
 if (s2 != NULL)
-{
 for (j = 0; s2[j] != '\0'; j++)
 ;
-}
 if (n < j)
+{
 j = n;
 k = i + j;
 k++;
 p = malloc(sizeof(char) * k);
+}
+else
+p = malloc(sizeof(char) *(i + n + 1));
 if (p == NULL)
+{
 return (NULL);
+}
 for (l = 0; l < i; l++)
 {
 p[l] = s1[l];
