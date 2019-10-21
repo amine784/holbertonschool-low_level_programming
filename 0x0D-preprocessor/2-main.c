6e7c23ed904fe321583befc1102463de12015adf
amine784 <1066@holbertonschool.com>
2019-10-21 05:45:33 -0700

    hedaer file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main -program that prints the name of the file it was
+*compiled from, followed by a new line.
+* Return: always success.
+**/
+int main(void)
+{
+  printf("%S\n",__FILE__);
+  return (0);
+}
