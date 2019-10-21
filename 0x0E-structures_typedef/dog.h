76abeaff94632d518d29ad5ee1ac52ca8652b07e
amine784 <1066@holbertonschool.com>
2019-10-21 09:02:42 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,7 +14,4 @@ float age;
 char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 #endif
