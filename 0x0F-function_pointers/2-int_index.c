a1fe268932f7851752744aaa08b5b0f7fc555e4a
amine784 <1066@holbertonschool.com>
2019-10-23 14:32:45 +0100

    no error

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,10 +13,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
 if ((array == NULL) || (size <= 0) || (cmp == NULL))
+return(-1);
 for (i = 0; i < size; i++)
 {
-  if((cmp)(array[i]) == 1)
-    return (i);
+if (cmp(array[i]) != 0)
+return (i);
 }
 return (-1);
 }
