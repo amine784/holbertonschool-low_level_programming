e8afa895dcf9c5f69b994d3e44b9ed18f1a8add5
amine784 <1066@holbertonschool.com>
2019-10-23 14:08:39 +0100

    pointer function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,9 +11,9 @@
 **/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-  size_t i;
-  if (action == NULL || array == NULL)
-    return;
-  for (i = 0; i < size; i++)
-    action(array[i]);
+size_t i;
+if (action == NULL || array == NULL)
+return;
+for (i = 0; i < size; i++)
+action(array[i]);
 }
