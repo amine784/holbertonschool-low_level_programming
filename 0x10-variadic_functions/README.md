ce1be9a0ea00d8b5c90967b9352e99ddaf72e3c3
amine784 <1066@holbertonschool.com>
2019-10-25 03:50:29 -0700

    vardiac function

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,8 @@
+vardiac function :
+Variadic functions are functions (e.g. printf) which take a variable number
+of arguments.
+
+The declaration of a variadic function uses an ellipsis as the last parameter,
+e.g. int printf(const char* format, ...);.
+See variadic arguments for additional detail on the syntax and
+automatic argument conversions. 
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef __VARDIAC_FUNCTIONS__
+#define __VARDIAC_FUNCTIONS__
+#include <stdarg.h>
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
