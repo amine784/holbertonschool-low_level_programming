5fc10574213e1a506aeb177a4aa33d0aa0ac911a
amine784 <1066@holbertonschool.com>
2019-10-27 17:10:05 +0100

    holberton

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,14 +1,13 @@
-global	main
-
-	section	.text
-main:	mov	rax, 1
-	mov	rdi, 1
-	mov	rsi, holberton
-	mov	rdx, 17
+global main
+	section.text
+main:	mov ebx, 1
+	mov rdi, 1
+	mov rsi, string
+	mov rdx, 17
 	syscall
-	mov	rax, 60
-	xor	rdi, rdi
-	syscall
+	mov rax, 60
+	xor rdi, rdi
+	int 0x80
 
-	section	.data
-holberton:db	"Hello, Holberton", 10
+section .data
+        string:	db "hello, Holberton",10
