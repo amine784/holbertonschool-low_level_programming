5b687dbe25f90369f6b4b7f4dd832f0796cadf7a
amine784 <1066@holbertonschool.com>
2019-10-27 05:17:43 +0100

    variadic functino

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,83 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*print_char-function that print char.
+*@arg:va_list type
+*Return :nothing.
+**/
+void print_char(va_list arg)
+{
+printf("%c", va_arg(arg, int));
+}
+/**
+* print_int-function that print integer.
+*@arg:va_list type
+*Return :nothing.
+**/
+void print_int(va_list arg)
+{
+printf("%d", va_arg(arg, int));
+}
+/**
+*print_float-function that print float .
+*@arg:va_list type
+*Return :nothing.
+**/
+void print_float(va_list arg)
+{
+printf("%f", va_arg(arg, double));
+}
+/**
+*print_string-function that print strings  .
+*@arg:va_list type
+*Return :nothing.
+**/
+void print_string(va_list arg)
+{
+char *c;
+c = va_arg(arg, char*);
+if (c == NULL)
+{
+c = "(nill)";
+}
+else
+printf("%s", c);
+}
+/**
+* print_all-function that print anything.
+*@format:format input.
+*Return:always success.
+**/
+void print_all(const char * const format, ...)
+{
+int i, j;
+va_list arg;
+char *separator = "";
+type op[] = {
+{"c", print_char},
+{"i", print_int},
+{"f", print_float},
+{"s", print_string},
+{NULL, NULL},
+};
+va_start(arg, format);
+i = 0;
+while (format && format[i])
+{
+j = 0;
+while (op[j].operator != NULL)
+{
+if (format[i] == op[j].operator[0])
+{
+printf("%s", separator);
+op[j].adressFunc (arg);
+separator = ", ";
+}
+j++;
+}
+i++;
+}
+printf("\n");
+va_end(arg);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,18 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+/**
+*struct tab_data-new type of array that contain operator and adress function.
+*@operator:pointer to adressfunction
+*@adressFunc:contain adress of first instruction of function used
+**/
+typedef struct tab_data
+{
+char *operator;
+void (*adressFunc)();
+} type;
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
 #endif
