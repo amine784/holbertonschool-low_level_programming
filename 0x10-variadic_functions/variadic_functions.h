47578ced126df5636a0f37129a0e4c5c43e023eb
amine784 <1066@holbertonschool.com>
2019-10-27 05:54:00 +0100

    holberton

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -16,8 +16,4 @@ typedef struct tab_data
 char *operator;
 void (*adressFunc)();
 } type;
-void print_char(va_list arg);
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
 #endif
