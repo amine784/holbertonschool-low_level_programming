8f71caab3a29ba985f68e12590e32bc0e69339ec
amine784 <1066@holbertonschool.com>
2019-10-31 21:04:20 +0100

    linked

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,13 @@
+global main
+	section.text
+main:	mov rax, 1
+	mov rdi, 1
+	mov rsi, string
+	mov rdx, 17
+	syscall
+	mov rax, 60
+	xor rdi, rdi
+	int 0x80
+
+section .data
+        string:	db "Hello, Holberton",10
