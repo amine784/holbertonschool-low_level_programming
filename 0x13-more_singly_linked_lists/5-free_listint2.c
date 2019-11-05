9e1209664b118ba548f7aa24db6acd7627ebac09
amine784 <1066@holbertonschool.com>
2019-11-05 04:57:28 +0100

    holberton

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+* free_listint2-function that free list
+*@head: first node.
+*Return always success
+**/
+void free_listint2(listint_t **head)
+{
+listint_t *a = NULL;
+int i;
+for (i = 0; *head != NULL; i++)
+{
+a = (*(*head)).next;
+free(*head);
+*head = a;
+}
+*head = NULL;
+}
