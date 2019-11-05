b4979fabba2ebcee211d9a11a2a6997caa739d89
amine784 <1066@holbertonschool.com>
2019-11-05 06:27:17 +0100

    new

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,6 +11,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 listint_t *elmt = NULL;
 listint_t *tempo = NULL;
+int i;
 elmt = malloc(sizeof(listint_t));
 if (elmt == NULL)
 {
@@ -21,7 +22,7 @@ return (NULL);
 tempo = *head;
 if (tempo != NULL)
 {
-while ((*tempo).next != NULL)
+for (i = 0;(*tempo).next != NULL; i++)
 tempo = (*tempo).next;
 (*tempo).next = elmt;
 return (elmt);
