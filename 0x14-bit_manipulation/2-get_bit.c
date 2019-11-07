b69418721bb894dc24c4cfe4513402cfcf3e1a1e
amine784 <1066@holbertonschool.com>
2019-11-07 18:07:10 +0100

    holberton

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,22 +10,18 @@
 **/
 int get_bit(unsigned long int n, unsigned int index)
 {
-unsigned long int a;
-a = 2;
-unsigned long int b;
+unsigned  int a;
+a = 1;
+unsigned  int b;
 b = (sizeof(n) * 8);
 if (index > b)
 {
 return (-1);
 }
-a -= 1;
 a <<= index;
 if (a & n)
 {
 return (1);
 }
-else
-{
-return (0);
-}
+ return(0);
 }
