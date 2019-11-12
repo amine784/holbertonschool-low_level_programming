1c865ecbcfec457515ca5e2254d136fb7b4e443e
amine784 <1066@holbertonschool.com>
2019-11-12 18:10:14 +0100

    holberton

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+#include <sys/types.h>
+/**
+* read_textfile-function that reads a text file and prints it
+*to the POSIX standard output.
+*@filename:name of file to print his contents.
+*@letters: size_t type number of chararc to print to the POSIX.
+*Return: Always success.
+**/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int ret = -1;
+int a;
+ssize_t R;
+ssize_t W;
+ssize_t ch;
+ch = letters;
+char *buffer;
+if (filename == NULL)
+{
+return (0);
+}
+a = open(filename,  O_RDWR);
+if (a == ret)
+{
+return (0);
+}
+buffer = malloc(ch * (sizeof(char)));
+if (buffer  == NULL)
+{
+return (0);
+}
+R = read(a, buffer, ch);
+if (R == ret)
+{
+return (0);
+}
+W = write(STDOUT_FILENO, buffer , R);
+if (W == ret ) 
+{
+return (0);
+}
+return (W);
+}
