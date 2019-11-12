8a77794442ffc2d1979c2e9cb0914ec3584f53df
amine784 <1066@holbertonschool.com>
2019-11-12 18:11:15 +0100

    holberton

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -2,6 +2,11 @@
 #define __FILE_IO__
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
