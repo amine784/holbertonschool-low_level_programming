729efe12ddfc365db700f0e8e7a1666ecedac753
amine784 <1066@holbertonschool.com>
2019-11-14 16:16:42 +0100

    holberton

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,14 +17,14 @@ int main(int argc, char *argv[])
 int rd = 1024, wr = 1024, c = 1024, from, to, c1 = 1024;
 char bf[1024];
 if (argc != 3)
-dprintf(STDERR_FILENO, "fatal error  "), exit(97);
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 from = open(argv[1], O_RDONLY);
 if (from == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file  %s", argv[1]);
 dprintf(STDERR_FILENO, "\n"), exit(98);
 }
-to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
 if (to == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write  to  file %s", argv[2]);
@@ -46,10 +46,10 @@ dprintf(STDERR_FILENO, "\n"), exit(99);
 }
 c = close(from);
 if (c < 0)
-dprintf(STDERR_FILENO, "Error: Can't close from %s\n",argv[1]), exit(100);
+dprintf(STDERR_FILENO, "Error: Can't close from %s\n", argv[1]), exit(100);
 c1 = close(to);
-if(c1 < 0)
-dprintf(STDERR_FILENO, "Error: Can't close to %s\n",argv[2]), exit(100);
+if (c1 < 0)
+dprintf(STDERR_FILENO, "Error: Can't close to %s\n", argv[2]), exit(100);
 }
 return (0);
 }
