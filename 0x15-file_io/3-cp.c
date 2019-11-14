8daacdbc6e433386b44ba09f9b894900a67d81c0
amine784 <1066@holbertonschool.com>
2019-11-14 14:48:11 +0100

    holberton

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,63 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+/**
+* main-main_function to copy buffer to anodher file.
+*@argc:number of arguments.
+*@argv:arguments lists.
+*Return:Always success.
+**/
+int main(int argc, char *argv[])
+{
+int rd = 1024, wr = 1024, c = 1024, from, to;
+char bf[1024];
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "fatal error  "), exit(97);
+}
+from = open(argv[1], O_RDONLY);
+if (from < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
+dprintf(STDERR_FILENO, "\n"), exit(98);
+}
+to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (to  < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't write  to  file %s", argv[2]);
+dprintf(STDERR_FILENO, "\n"), exit(99);
+}
+while (rd == 1024)
+{
+rd = read(from, bf, 1024);
+if (from < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
+dprintf(STDERR_FILENO, "\n"), exit(98);
+}
+wr = write(to, bf, rd);
+if (wr < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't write  to  file %s", argv[2]);
+dprintf(STDERR_FILENO, "\n"), exit(99);
+}
+c = close(from);
+if (c < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't close %s", argv[1]);
+dprintf(STDERR_FILENO, "\n");
+exit(100);
+}
+c = close(to);
+if (c < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't close %s", argv[2]);
+dprintf(STDERR_FILENO, "\n"), exit(100);
+}}
+return (0);
+}
