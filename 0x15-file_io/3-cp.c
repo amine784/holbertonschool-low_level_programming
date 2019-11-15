60e3b8ec58d664ec7e7c0b17f5d325becd88a0cb
amine784 <1066@holbertonschool.com>
2019-11-15 21:25:18 +0100

    holberton

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,19 +21,19 @@ dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 from = open(argv[1], O_RDONLY);
 if (from == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file  %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
 to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (to == -1)
-dprintf(STDERR_FILENO, "Error: Can't write  to %s\n", argv[2]), exit(99);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 rd = read(from, bf, 1024);
 while (rd > 0)
 {
 wr = write(to, bf, rd);
 if (wr == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write  to  file %s\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 exit(99);
 }
 rd = read(from, bf, 1024);
