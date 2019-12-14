1289680e025028b873dda05a838b554bb47a52ea
amine784 <1066@holbertonschool.com>
2019-12-14 12:18:39 +0100

    holberto

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,9 +10,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 dlistint_t *node;
 node = malloc(sizeof(dlistint_t));
 if (node == NULL)
-{
 return (NULL);
-}
 (*node).n = n;
 (*node).next = *head;
 (*node).prev = NULL;
