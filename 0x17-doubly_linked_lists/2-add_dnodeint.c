a184746e6241fc22b2bde6a4cbde552e2f3eaae1
amine784 <1066@holbertonschool.com>
2019-12-14 13:22:51 +0100

    first

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -4,28 +4,16 @@
 *@head first node of :doubly linked lists.
 *@n:value of new node
 *Return:Always success.
-*dlistint_t *add_dnodeint(dlistint_t **head, const int n)
-*{
-*dlistint_t *node;
-*node = malloc(sizeof(dlistint_t));
-*if (node == NULL)
-*return (NULL);
-*(*node).n = n;
-*(*node).next = *head;
-*(*node).prev = NULL;
-*head = node;
-*return (node);
-*}
 **/
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *new;
-new = malloc(sizeof(dlistint_t));
-if (new == NULL)
+dlistint_t *node;
+node = malloc(sizeof(dlistint_t));
+if (node == NULL)
 return (NULL);
-new->n = n;
-new->next = *head;
-new->prev = NULL;
-*head = new;
-return (new);
+(*node).n = n;
+(*node).next = *head;
+(*node).prev = NULL;
+*head = node;
+return (node);
 }
