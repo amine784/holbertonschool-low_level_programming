8e5d1eaf62296323fcd661270e34d18d625c0a46
amine784 <1066@holbertonschool.com>
2019-12-14 09:05:27 +0100

    holberto

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include"lists.h"
+/**
+* *add_dnodeint-function that a node begin of doubly list elements.
+*@head first node of :doubly linked lists.
+*@n:value of new node
+*Return:Always success.
+**/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+int buffer = sizeof(dlistint_t);
+dlistint_t *node = malloc(buffer);
+if (node == NULL)
+{
+printf("can't allocate memory");
+}
+(*node).n = n;
+(*node).next = *head;
+*head = node;
+(*node).prev = NULL;
+return (node);
+}
