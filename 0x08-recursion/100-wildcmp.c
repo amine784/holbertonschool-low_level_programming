03a8851e1053a9fa7de3cb0db03db49d8da72058
amine <1066@holbertonschool.com>
2019-12-16 04:41:00 -0800

    holberto

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -9,14 +9,20 @@
 int wildcmp(char *s1, char *s2)
 {
 char delim = '\0';
-int c;
+int a, b, c;
 char *S1 = s1 + 1, *S2 = s2 + 1;
 if (*s1 == delim  && *s2 == delim)
 return (1);
 if (*s1 == *s2)
-return (wildcmp(S1, S2));
+{
+a = wildcmp(S1, S2);
+return (a);
+}
 if (*s1 == delim && *s2 == delim)
-return (wildcmp(s1, S2));
+{
+b = wildcmp(s1, S2);
+return (b);
+}
 if (*s2 == '*')
 {
 c = (wildcmp(s1, S2)) || (wildcmp(S1, s2));
