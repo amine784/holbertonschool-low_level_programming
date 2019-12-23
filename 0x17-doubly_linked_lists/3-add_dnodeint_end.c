1b5e16f82c7662fbc594a0d7aa10eb33e8689d79
amine784 <1066@holbertonschool.com>
2019-12-23 13:13:58 +0100

    rec

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+*_*add_dnodeint_end-function that add node at the end of list.
+*@head:pointer refers to yhe first node il list
+*@n:int type,data of the new node
+*Return: Always success
+**/
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+dlistint_t *node;
+dlistint_t *h;
+node = malloc(sizeof(dlistint_t));
+if (node == NULL)
+{
+printf("can't allocate memory");
+exit(0);
+}
+(*node).n = n;
+(*node).next = NULL;
+(*node).prev = NULL;
+if (*head == NULL)
+{
+*head = node;
+return (0);
+}
+h = *head;
+while ((*h).next != NULL)
+{
+h = (*h).next;
+}
+(*h).next = node;
+(*node).prev = h;
+return (node);
+}
