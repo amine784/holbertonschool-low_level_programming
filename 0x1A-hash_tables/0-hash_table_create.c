3c76b9e21b619e15442b7c4aca411865e902b537
amine784 <1066@holbertonschool.com>
2020-01-18 17:24:20 +0100

    holberto

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,19 +9,13 @@
 **/
 hash_table_t *hash_table_create(unsigned long int size)
 {
-if (size == 0)
-{
-return (NULL);
-}
-hash_table_t *hash_table;
+hash_table_t *hash_table = NULL;
 hash_table = malloc(sizeof(hash_table_t));
 if (hash_table == NULL)
-{
 return (NULL);
-}
 (*hash_table).size = size;
 (*hash_table).array = malloc(sizeof(hash_node_t *) * size);
-if ((*hash_table).array == NULL)
+if (!((*hash_table).array))
 {
 free(hash_table);
 return (NULL);
