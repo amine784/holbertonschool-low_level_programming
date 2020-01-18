4fd265b2f4ae5920a7fa5802ed8c64481a2bc575
amine784 <1066@holbertonschool.com>
2020-01-18 17:00:34 +0100

    scnd

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 /**
 * *hash_table_create- a function that creates a hash table.
@@ -19,7 +20,7 @@ if (hash_table == NULL)
 return (NULL);
 }
 (*hash_table).size = size;
-(*hash_table).array = malloc(sizeof(hash_node_t) * size);
+(*hash_table).array = malloc(sizeof(hash_node_t *) * size);
 if ((*hash_table).array == NULL)
 {
 free(hash_table);
