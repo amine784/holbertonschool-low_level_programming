fef64dd103c2948e1aa1d16a6a304ce3c39ebcf0
amine784 <1066@holbertonschool.com>
2020-01-19 18:07:55 +0100

    holberto

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -12,7 +12,7 @@ void hash_table_print(const hash_table_t *ht)
 unsigned long int i;
 char *spec = "";
 hash_node_t *p_node;
-if (ht == NULL  || (*ht).array == NULL)
+if (ht == NULL  || (*ht).array == NULL || (*ht).size == 0)
 {
 printf("{}\n");
 return;
