9e1348d9bb60c326acca9b62c287ec1a44471bfd
amine784 <1066@holbertonschool.com>
2020-01-19 15:43:21 +0100

    holberton

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
 **/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-unsigned long int index;
+unsigned long int i;
 hash_node_t *node;
 node = malloc(sizeof(hash_node_t));
 if ((node == NULL) || (value == NULL) || ht == NULL)
@@ -20,11 +20,11 @@ return (0);
 }
 else if (key)
 {
-index = key_index((const unsigned char *)key, ht->size);
-node->key = strdup(key);
-node->value = strdup(value);
-node->next = ht->array[index];
-ht->array[index] = node;
+i = key_index((const unsigned char *)key, (*ht).size);
+(*node).value = strdup(value);
+(*node).key = strdup(key);
+(*node).next = (*ht).array[i];
+(*ht).array[i] = node;
 return (1);
 }
 return (0);
