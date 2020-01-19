4d9baba902b2b0a50a07663a4bf5b22f1b25ab25
amine784 <1066@holbertonschool.com>
2020-01-19 17:15:49 +0100

    holberto

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 unsigned long int i;
 hash_node_t *node;
 node = malloc(sizeof(hash_node_t));
-if (!ht || !key || (value == NULL) || !node)
+if ((node == NULL) || (value == NULL) || ht == NULL)
 {
 return (0);
 }
