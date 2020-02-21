25e96e200117b319a447b4b7cae0ba2c74494939
amine784 <1066@holbertonschool.com>
2020-02-21 20:07:55 +0100

    last

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''perimetre'''
+
+
+def island_perimeter(grid):
+    '''def function island per'''
+    peri = 0
+    l = len(grid)
+    for i in range(l):
+        return (peri)
